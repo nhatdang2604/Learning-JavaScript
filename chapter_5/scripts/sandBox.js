a8e43d549c049c141dd3cf01d15bd10665ffd6bf
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-01 00:00:48 +0700

    Finish introduce to Math object

--- chapter_5/scripts/sandBox.js
+++ chapter_5/scripts/sandBox.js
@@ -57,3 +57,19 @@ console.log(typeof(user));
 user.login();
 user.logout();
 user.logBlogs();
+
+// Math object
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 5.2;
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+//random numbers
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random * 100));
