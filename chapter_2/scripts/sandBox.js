d6da6aa8b84644833826c7088b045dee0438c411
nhatdang2604 <nhatdang2604@gmail.com>
2022-06-30 14:14:57 +0700

    Add lastIndexOf testing

--- chapter_2/scripts/sandBox.js
+++ chapter_2/scripts/sandBox.js
@@ -1,1 +1,47 @@
-alert("Hello World")
+let age = 25;
+let year = 2022;
+
+
+//log things to console
+console.log(age, year);
+
+age = 30;
+console.log(age, year);
+
+const point = 100;
+console.log(point);
+
+var score = 75;
+console.log(score);
+
+//strings
+console.log("Hello World");
+
+let email = "test@gmail.com";
+console.log(email);
+
+//String concatenation
+let firstName = "John";
+let lastName = "Doe";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//Getting characters
+console.log(fullName[0]);
+
+//String length
+console.log(fullName.length);
+
+//String methods
+console.log(fullName.toUpperCase());
+console.log(fullName);
+
+let result = fullName.toLowerCase();
+console.log(result);
+
+let index = email.indexOf("@");
+console.log(index);
+
+result = email.lastIndexOf("t");
+console.log(reuslt);
