96205b17e7aef792abe847aecba20c64d7e1ef60
nhatdang2604 <nhatdang2604@gmail.com>
2022-06-30 16:07:41 +0700

    Finish while loop

--- chapter_3/scripts/sandBox.js
+++ chapter_3/scripts/sandBox.js
@@ -17,3 +17,16 @@ for (let i = 0; i < names.length; ++i) {
     let html = `<div>${names[i]}</div>`;
     console.log(html);
 }
+
+//while loops
+let i = 0;
+while (i < 5) {
+    console.log("in loop: " + i);
+    ++i;
+}
+
+i = 0;
+while (i < names.length) {
+    console.log(names[i]);
+    ++i;
+}
