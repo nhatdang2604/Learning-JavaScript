ce295c8864c1a005f76b5840a6d1ca9a8b120b34
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-01 16:08:16 +0700

    Finish using .querySelector() and .querySelectorAll()

---
+++ chapter_6/scripts/sandBox.js
@@ -0,0 +1,12 @@
+// const paragraph = document.querySelector("body > h1");
+// console.log(paragraph);
+
+const paragraphs = document.querySelectorAll("div");
+paragraphs.forEach(para => {
+    console.log(para);
+})
+
+const errors = document.querySelectorAll(".error");
+errors.forEach(error => {
+    console.log(error);
+})
