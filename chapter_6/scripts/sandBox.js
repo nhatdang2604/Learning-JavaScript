87d6c0115d56da3d8de2037da3e06afd1a10c014
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-01 16:15:37 +0700

    Finish using .getElementById(), .getElementsByClassName and .getElementsByTagName()

--- chapter_6/scripts/sandBox.js
+++ chapter_6/scripts/sandBox.js
@@ -1,12 +1,26 @@
-// const paragraph = document.querySelector("body > h1");
-// console.log(paragraph);
+//Using .querySelector()
+const paragraph = document.querySelector("body > h1");
+console.log(paragraph);
 
+//Using .querySelectorAll()
 const paragraphs = document.querySelectorAll("div");
 paragraphs.forEach(para => {
     console.log(para);
 })
 
-const errors = document.querySelectorAll(".error");
-errors.forEach(error => {
-    console.log(error);
-})
+// const errors = document.querySelectorAll(".error");
+// errors.forEach(error => {
+//     console.log(error);
+// })
+
+//Get an element by id
+const title = document.getElementById("page-title");
+console.log(title);
+
+//Get elements by class name
+const errors = document.getElementsByClassName("error");
+console.log(errors);
+
+//Get elements by tag name
+const paras = document.getElementsByTagName("p");
+console.log(paras);
