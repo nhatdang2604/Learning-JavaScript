7f280bc219a999de935dd0297700a40913da8554
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-05 13:10:14 +0700

    Finish ordinary function

---
+++ from_the_book/chapter_25_Callables/chap_25.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//Ordinary functions
+
+//Function declaration
+function foo0(a, b) {
+    //do something
+}
+
+//Function expression
+const foo1 = function(a, b) {
+    //do something
+}
