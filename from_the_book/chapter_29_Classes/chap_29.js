fdeab92ee5d9ae9da083049ecb9736c7c6da1ed1
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-06 20:37:39 +0700

    Add person

--- from_the_book/chapter_29_Classes/chap_29.js
+++ from_the_book/chapter_29_Classes/chap_29.js
@@ -1,1 +1,17 @@
 "use strict";
+
+class Person {
+    #_firstName;
+    constructor(firstName) {
+        this.#_firstName = firstName;
+    };
+    
+    describe() {
+        return `Person names #${this.#_firstName}}`;
+    }
+
+    static extractNames(people) {
+        return people.map(person => person.#firstName);
+    }
+}
+
