4f3340111720293fee32e1f306fd07d2d3ecf292
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-06 20:50:03 +0700

    Add Conatiner

--- from_the_book/chapter_29_Classes/chap_29.js
+++ from_the_book/chapter_29_Classes/chap_29.js
@@ -10,8 +10,32 @@ class Person {
         return `Person names #${this.#_firstName}}`;
     }
 
+    firstName() {return this.#_firstName;}
+
     static extractNames(people) {
-        return people.map(person => person.#firstName);
+        return people.map(person => person.#_firstName);
     }
 }
 
+const Tuấn = new Person("Tuấn");
+console.log(Tuấn.firstName());
+const studentSize = 5;
+let people = [];
+for (let i = 0; i < studentSize; ++i) {
+    people.push(new Person("Test" + i));
+};
+
+let names = Person.extractNames(people);
+for (name of names) {
+    console.log(name);
+}
+
+//Add attribute in constructor
+class Container {
+    constructor(value) {
+        this.value = value;
+    }
+}
+
+const cont = new Container(12);
+console.log(cont.value);
