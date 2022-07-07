b367d87c8b737acfc63a5153083d228177a11cbb
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-07 21:19:23 +0700

    Add basic way to iterate over generation

--- from_the_book/chapter_38_Synchronous generators/chap_38.js
+++ from_the_book/chapter_38_Synchronous generators/chap_38.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let loc = 0;
+const generator = function* () {
+    loc = 1; yield {x: 0, y: 0};
+    loc = 2; yield {x: 1, y: 1};
+    loc = 3; yield {x: 2, y: 2};
+};
+
+const iter = generator();
+let value = iter.next();
+while (!value.done) {
+    console.log(value);
+    value = iter.next();
+    value = iter;
+}
