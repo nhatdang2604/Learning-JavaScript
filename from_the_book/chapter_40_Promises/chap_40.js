a52f83d7736c6321c7658f82d5f93fe7a8a1a906
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-08 15:33:40 +0700

    Add example for promise

--- from_the_book/chapter_40_Promises/chap_40.js
+++ from_the_book/chapter_40_Promises/chap_40.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const addAsync = function(x, y) {
+    return Promise.resolve()
+        .then(() => {
+            return x + y;
+        })
+};
+
+let result = 
+    addAsync(1,2)
+    .then((result) => {
+        return result + 3;
+    });
+
+console.log(result);
